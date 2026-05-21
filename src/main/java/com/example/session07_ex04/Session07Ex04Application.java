b2afb210aaf4a2e6f1710117dd52d5d9e4366fb2
ZxineReal <stargolem90@gmail.com>
2026-05-21 21:59:34 +0700

    upload

--- src/main/java/com/example/session07_ex04/Session07Ex04Application.java
+++ src/main/java/com/example/session07_ex04/Session07Ex04Application.java
@@ -2,8 +2,10 @@ package com.example.session07_ex04;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class Session07Ex04Application {
 
     public static void main(String[] args) {
